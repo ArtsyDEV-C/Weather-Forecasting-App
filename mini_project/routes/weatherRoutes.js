2c3c2bc3e9afded57171fbb82d25d0ca3f2e09d7
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-03-14 01:31:06 +0530

    Delete mini_project directory

--- mini_project/routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getWeather } = require('../controllers/weatherController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/:city', authMiddleware, getWeather);
-
-module.exports = router;
